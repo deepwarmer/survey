7c7a2d83ca54b77284fc44f2f4387d4f040297f8
deepworm <deepworm@qq.com>
2020-03-27 20:21:54 +0800

    I don't know, everything is updated to team

--- survey/SurveyServer/survey/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('create',views.create,name='create'),
-    path('getsurvey/<int:survey_id>',views.getSurvey,name='getSurvey'),
-    path('savesurvey',views.saveSurvey,name='saveSurvey')
-]
