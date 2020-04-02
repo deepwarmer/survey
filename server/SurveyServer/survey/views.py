fa9a18cc886e861b2d67624e947a6f80b1dee5f6
deeworm <deepworm@qq.com>
2020-04-02 16:43:51 +0800

    server completed

--- server/SurveyServer/survey/views.py
+++ server/SurveyServer/survey/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import *
+import shutil
+import wordcloud
 import json
 # Create your views here.
 def create(request):
@@ -35,9 +37,65 @@ def getSurvey(request,survey_id):
     #直接返回文本
     return HttpResponse(objSurvey.jsonContent)
 
-def submitSurvey(request,survey_id):
-    result=json.loads(request.POST['content'])
-    surveyid=1
+submit_sample_json='''
+{
+  "surveyId": 19,
+  "length": 3,
+  "data": [
+    {
+      "type": "radio",
+      "question": "What's your gender?",
+      "option": { "1": "male" }
+    }, 
+    {
+      "type": "checkbox",
+      "question": "What smartphone brands do you like?",
+      "option": { "1": "Huawei", "2": "Xiao Mi" }
+    },
+    {
+      "type": "text",
+      "question": "What do you care aboud most when buying a smartphone?",
+      "option": { "1": "appearance" }
+    }
+  ]
+}
+'''
+
+def submitSurvey(request):
+    request.encoding='utf-8'
+    # if True:
+        # result=json.loads(submit_sample_json)
+    if request.method=="POST" and request.POST['content']:
+    # if request.method=="POST" :
+        # return HttpResponse("t1")
+        result=json.loads(request.POST['content'])
+        survey_id=result['surveyId']
+        for question in result['data']:
+            strType=question['type']
+            if strType=='radio':
+                objSingleQuestion=clsSingleQuestion.objects.filter(survey=survey_id,strDescription=question['question'])[0]
+                objSingleOption=clsSingleOption.objects.filter(question=objSingleQuestion.id,strDescription=question['option']['1'])[0]
+                objSingleOption.count=objSingleOption.count+1
+                objSingleOption.save()
+            elif strType=='checkbox':
+                objCheckboxQuestion=clsCheckboxQuestion.objects.filter(survey=survey_id,strDescription=question['question'])[0]
+                tmp={}
+                for option in question['option'].values():
+                    objCheckboxOption=clsCheckboxOption.objects.filter(question=objCheckboxQuestion.id,strDescription=option)[0]
+                    objCheckboxOption.count =objCheckboxOption.count+1
+                    objCheckboxOption.save()
+
+            elif strType=='text':
+                objTextQuestion=clsTextQuestion.objects.filter(survey=survey_id,strDescription=question['question'])[0]
+                objTextAnswer=clsTextAnswer()
+                objTextAnswer.strAnswer=question['option']['1']
+                objTextAnswer.question=objTextQuestion
+                objTextAnswer.save()
+                
+            else :return HttpResponse("Wrong question type.")
+        return HttpResponse("success")
+    else:
+        return HttpResponse("You should POST survey result in content")
 def getSurveyResult(survey_id):
     res={"id":survey_id}
     res['questions']=[]
@@ -68,6 +126,14 @@ def getSurveyResult(survey_id):
         for answer in objAnswers:
             strQuestion["answers"].append(answer.strAnswer)
         res["questions"].append(strQuestion)
+        
+        strTot=""
+        for s in strQuestion['answers']:strTot+=s+' '
+        wdcld=wordcloud.WordCloud(background_color="white")
+        wdcld.generate(strTot)
+        wdcld.to_file("wdcld_qid_"+str(question.id)+".png")
+        shutil.move("wdcld_qid_"+str(question.id)+".png","survey/static/survey/imgs/wdcld_qid_"+str(question.id)+".png")
+
     res["questions"]=sorted(res['questions'],key=id)
     return json.dumps(res)
 def showSurvey(request,survey_id):
